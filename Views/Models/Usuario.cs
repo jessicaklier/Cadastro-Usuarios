b4abcd95b402d2c8acc3e863c3d94ebd8904c35e
jessicaklier <jhessicadesouza12@hotmail.com>
2023-09-22 15:16:19 -0300

    CadastroDeUsuarios

---
+++ Views/Models/Usuario.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Views.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        private static List<Usuario> listagem = new List<Usuario>();
+
+        public static IQueryable<Usuario> Listagem
+        {
+            get
+            {
+                return listagem.AsQueryable();
+            }
+        }
+        static Usuario()
+        {
+            Usuario.listagem.Add(new Usuario { IdUsuario = 1, Nome = "Jessica", Email = "jhessica@gmail.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 2, Nome = "Terezinha", Email = "terezinha@gmail.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 3, Nome = "Jeferson", Email = "jeferson@gmail.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 4, Nome = "Juliano", Email = "juliano@gmail.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 5, Nome = "Jean", Email = "jean@gmail.com" });
+
+        }
+
+        public static void Salvar(Usuario usuario)
+        {
+            var usuarioExistente = Usuario.listagem.Find(u => u.IdUsuario == usuario.IdUsuario);
+            if(usuarioExistente != null)
+            {
+                usuarioExistente.Nome = usuario.Nome;
+                usuarioExistente.Email = usuario.Email;
+            }
+            else
+            {
+                int maiorId = Usuario.Listagem.Max(u => u.IdUsuario);
+                usuario.IdUsuario = maiorId + 1;
+                Usuario.listagem.Add(usuario);
+            }
+        }
+        public static void Excluir(int idusuario)
+        {
+            var usuarioExistente = Usuario.listagem.Find(u => u.IdUsuario == idusuario);
+            if(usuarioExistente != null)
+            {
+                Usuario.listagem.Remove(usuarioExistente);
+            }
+        }
+    }
+}
+
---
+++ Views/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Views
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
