a3376dd7ddc9ee2b1d596c52ab866de8405b117b
jessicaklier <jhessicadesouza12@hotmail.com>
2023-09-25 14:14:54 -0300

    Atualizando as páginas de Layout

--- Views/Controllers/HomeController.cs
+++ Views/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Views.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.QtdeUsuarios = Usuario.Listagem.Count();
             return View();
         }
         [HttpGet]
